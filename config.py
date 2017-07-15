8a8b6f271e8fc0685f46cdc18e6b0fd0fd66801a
yuyumaz <yuyumaz@stanford.edu>
2017-07-15 18:18:50 -0400

    added config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = 'nt3grG0IsSNnFTMhDjK1xDZcx'
+consumer_secret = 'QOrFDNyYH7df4jdCQq9Q2B6gWlGpYAubgTyHAqH4bE8blBjGnp'
+access_token = '212615999-ArxHrY3aifWI8CPgpZ5tz8974KbEfvjcYg2OiAF2'
+access_secret = 'J98KAqzKukhKI8cpF8NroE4VKmpOjZtK1gRqGS8b9Q4XL'
